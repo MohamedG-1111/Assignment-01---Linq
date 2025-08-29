c2c0c2966a3600c8274f49b20ad0977b4aef1115
Mohamed <Mohamed@1111wwe@gmail.com>
2025-08-29 14:51:28 +0300

    Add project files.

---
+++ Assignment 01 - Linq/Program.cs
@@ -0,0 +1,12 @@
+﻿using Assignment_01___Linq.Data;
+
+namespace Assignment_01___Linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
